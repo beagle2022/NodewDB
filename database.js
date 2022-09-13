ca69e9a36b8e8a56075230953ef138cd5502bfd2
Abhishek Bhattachaya <ab7607@rediffmail.com>
2022-09-13 17:11:09 +0530

    Troubleshooting error

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql=require('mysql');
+
+const conn=mysql.createConnection;
+
+host: 'localhost'
+database: 'node_twitter'
+user: 'root'
+password: 'MyBeagl7&'
+
+module.exports = conn;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ex= require('express');
 const my= require('mysql');
 const app= ex();
-
+const connection=require('./database');
 
 app.get('/', (req,res)=>{
 
@@ -11,4 +11,9 @@ app.get('/', (req,res)=>{
 
 app.listen(3001, ()=> {
     console.log('App listening on port 3001');
+    conn.connect(function(err){
+
+        if(err) throw err;
+        console.log('Database Connected');
+    });
 });
