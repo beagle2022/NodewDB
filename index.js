26b7a9e95548772d27610e2670ac254492bde828
Abhishek Bhattachaya <ab7607@rediffmail.com>
2022-09-13 16:13:38 +0530

    Check Port

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const ex= require('express');
 const my= require('mysql');
 const app= ex();
 
+
+app.get('/', (req,res)=>{
+
+    res.send('Hey there');
+
+});
+
 app.listen(3001, ()=> {
     console.log('App listening on port 3001');
 });
