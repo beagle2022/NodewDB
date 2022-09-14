4a973316f24b285462df0d3ca13f4516de7297a6
Abhishek Bhattachaya <ab7607@rediffmail.com>
2022-09-14 12:35:43 +0530

    Final database select fetch commit

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app= express();
 const connection=require('./database');
 
 app.get('/', (req,res)=>{
-
-    res.send('Hey there');
-
+    let sql="Select * FROM node_twitter.myusers";
+    connection.query(sql,(err,results)=>{
+        if(err) throw err;
+        res.send(results);
+    });
+    
 });
 
 app.listen(3010, ()=> {
